edf733b0985123b2be2f9adf751a05e7ef518386
Peter Kristensen <peter@ptx.dk>
2024-10-10 11:09:29 +0200

    Add test

---
+++ e2e/smoke/hello_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	bs, err := os.ReadFile("hello.wasi")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(bs) < 10 {
+		t.Fatal("file is too short")
+	}
+}
